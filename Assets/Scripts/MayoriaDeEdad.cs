2ae6ab748a2c49fe75295fa82ad5040b2d09780d
frantute <102159238+frantute@users.noreply.github.com>
2022-04-24 16:54:36 -0300

    Initial commit

---
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MayoriaDeEdad : MonoBehaviour
+{
+    public int edadusuario;
+   
+    void Start()
+    {
+        if (edadusuario >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+        
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+          
+        }
+    }
+}
+
